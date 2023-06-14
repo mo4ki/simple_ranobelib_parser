544617f6e46e5fef624a2ef5ff16f6de26a4fdc0
mo4ki <55511635+mo4ki@users.noreply.github.com>
2023-06-14 17:12:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+from utils import *
+from configs import *
+
+# создаём папки и файлы, нужные для работы
+install()
+
+print('url: ' + root + '\n\n')
+
+# парсим главы
+for non_changed_charapter in charapters:
+
+    try:
+    
+        print('parsing...    ', end = '')
+        
+        # заменяем / на -, т.к / нельзя использовать в названии папки
+        charapter = non_changed_charapter.replace("/","-")
+        
+        # меняем рабочую дерикторию
+        change_dir(charapter) 
+        
+        # генерируем url главы, которую парсим
+        url = root + non_changed_charapter + translate
+        html = download_page(url)
+        
+        #парсим
+        parser = text_parser()
+        parser.images = images
+        parser.feed(html)
+        
+    except stop_parse as e:
+        print(charapter + '    ' + str(e))
+        
+    except Exception as e:
+        print(e)
+        
+    except KeyboardInterrupt:
+        print('\n зачем прерывать процесс?')
+        exit()
---
+++ utils.py
@@ -0,0 +1,92 @@
+from html.parser import HTMLParser
+import os
+import requests
+
+# класс уникальной ошибки 
+class stop_parse(Exception):
+    def __init__(self, text):
+        self.txt = text
+
+# класс, который парсит страницы 
+class text_parser(HTMLParser):
+    
+    # флаг, что бы понять, когда начался текст
+    flag = False
+    
+    # флаг на парсинг картинок
+    images = True
+    
+    # метод, который 
+    def handle_starttag(self, tag, attrs):
+        
+        # здесь текст начинается. Ставим флажок
+        if ('class', 'reader-container container container_center') in attrs:
+            self.flag = True
+        
+        # если в тектсе попалась картинка
+        if self.flag and self.images and tag == "img":
+            download_file(attrs[2][1])
+        
+    def handle_data(self, data):
+        
+        if "Реклама" in data: # прекращаем парс на словах
+            raise stop_parse(" parsed!")
+            
+        if self.flag: # парсим текст
+            
+            data = data.replace('\n', '') # чистим текст
+            write_text(data)
+            
+
+# cкачать html страницы
+def download_page(url):
+
+    request = requests.get(url, headers = {"User-Agent": "M"})
+    text = request.text
+    
+    return text
+
+# скачать файл и записать его
+def download_file(url):
+    
+    request = requests.get(url, allow_redirects=True)
+
+    filename = os.path.basename(url)
+
+    file = open(filename, 'wb')
+    file.write(request.content)
+    
+    file.close()
+
+# записываем текст из главы в файл
+def write_text(text):
+
+    file = open("text.txt", "a", encoding = "utf-8")
+    file.write(text + "\n\n")
+    
+    file.close()
+    
+# создаём файлы и папки нужные для работы
+def install():
+    
+    # если не существует root файла
+    if not os.path.exists('charapters/root'): 
+    
+        os.mkdir('charapters')
+        open('charapters/root', 'a').close()    
+    
+    os.chdir('charapters')
+    
+# меняем текущую дерикторю на дерикторию другой главы
+def change_dir(charapter):
+
+    if 'root' not in os.listdir(): 
+        os.chdir('..')
+        
+    if os.path.exists(charapter):
+        raise stop_parse(" was alredy parsed")
+        
+    os.mkdir(charapter)
+    os.chdir(charapter)
+    
+    
